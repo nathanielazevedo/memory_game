021e730d1fde90e139e250de7ef905faa8489d92
nathanielazevedo <nathanielazevedo@hotmail.com>
2020-07-14 18:32:28 -0400

    changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 //later we use this to append newDiv's to it.
 const gameContainer = document.getElementById("game");
+let counter = document.createElement('h3');
 
 //the array of colors we're shuffling now.
 const COLORS = ['olivedrab', 'mediumpurple', 'indianred', 'teal', 'olivedrab', 'mediumpurple', 'indianred', 'teal', 'peru', 'peru']
@@ -43,14 +44,14 @@ function createDivsForColors(colorArray) {
     // append the div to the element with an id of game
     gameContainer.append(newDiv);   
   }
+
   title.style.display = 'none';
   startButton.style.display = 'none';
 
-//to count the number of clicks the user has done.
-  let counter = document.createElement('h1');
-    counter.innerHTML = count;
-    counter.classList.add('counters');
-    gameContainer.append(counter);
+  
+  counter.innerText = count;
+  counter.classList.add('count');
+  gameContainer.append(counter);
 }
 
 //to remember cards and total flipped.
@@ -59,75 +60,87 @@ let card2 = 0;
 let card1color = 0;
 let count = 0;
 let flipped = 0;
+let noClicking = 0;
 
-// TODO: Implement this function!
+// clicks
 function handleCardClick(event) {
 
-//i'm saving access to each card each flip.
-  let thisCard = event.target;
-  let counters = document.getElementsByClassName('counters');
+  if(noClicking == 1){
+    console.log('noClicking');
+    return;}
   
-//if they're trying to choose a third card quickly or if they're clicking the same card.
-  if(card1 && card2 || card1 == thisCard) return;
-
-
+  count++;
+  counter.innerText = count;
+  let thisCard = event.target;
+  
 //if this is the first click.
-  else if(!card1 && !card2){ 
+  if(card1 == 0){ 
     thisCard.style.backgroundColor = thisCard.classList;
     card1 = thisCard;
-    card1color = event.target.style.backgroundColor;
-    count++;
-    counters[0].innerText = count;
+    card1color = thisCard.classList.value;
+    console.log('no card 1');
+    card1.removeEventListener('click', handleCardClick);
   }
 
-//if this is second card and they're different.
-  else if(thisCard.classList != card1color && card1 && !card2){   
+//if there is a card1
+  else{
+    console.log('there is a card1');
+    noClicking = 1;
     thisCard.style.backgroundColor = thisCard.classList;
     card2 = thisCard;
-    count++;
-    counters[0].innerText = count;
-    setTimeout(function(){
-      card1.style.backgroundColor = 'black';
-      thisCard.style.backgroundColor = 'black';
-      card1 = 0;
-      card2 = 0;
-      }, 1000);
-  }
 
-//if this is second card and they're the same.
-  else if(thisCard.classList == card1color && card1 && !card2){
-    thisCard.style.backgroundColor = thisCard.classList;
-    card2 = thisCard;
-    count++;
-    flipped = flipped + 2;
-    counters[0].innerText = count;
-    thisCard.removeEventListener('click', handleCardClick);
-    card1.removeEventListener('click', handleCardClick)
-    setTimeout(function(){
-      card1 = 0;
-      card2 = 0;
-      }, 1000);
-  }
- 
-//to check if the player has won the game.
-  if(flipped == (COLORS.length) ) {
 
-//if they did get the new high score.
-    if(localStorage.getItem('low-score') > count){
-      gameContainer.innerHTML = `<span id="congrats" >GREAT JOB! </span> <span id="yourScore"> You got the new best score of ${count} </span> <span onclick="window.location.reload()" id="reload"> NewGame</span>`;
-      localStorage.setItem('low-score', count);
-     }
+    if(card2.classList.value != card1color){
+      setTimeout(function(){
+          console.log('card2 does not match card1')
+          card1.style.backgroundColor = 'black';
+          thisCard.style.backgroundColor = 'black';
+          card1.addEventListener('click', handleCardClick);
+          card1 = 0;
+          card2 = 0;
+          noClicking = 0;
+          }, 1000);
+    }
+  
+      else{
+          console.log('card2 matches card1')
+          flipped += 2;
+          card2.removeEventListener('click', handleCardClick);
+          card1.removeEventListener('click', handleCardClick);
+          setTimeout(function(){
+              card1 = 0;
+              card2 = 0;
+              noClicking = 0;
+          }, 1000);
+      
 
-//if they did not get the new high score.
-    else{
-    gameContainer.innerHTML = `<span id="congrats" >GOOD JOB! </span> <span id="yourScore"> Your score: ${count} <br> Low score: ${localStorage.getItem('low-score')} </span> <span onclick="window.location.reload()" id="reload"> New Game</span> `;}
+        //to check if the player has won the game.
+          if(flipped == (COLORS.length)){
 
-    const congra = document.querySelector('#congrats');   
-    setInterval(function(){  
-      congra.style.color = randomRGB(); 
-    }, 1000);
-  }
-}
+        //if they did get the new high score.
+            if(localStorage.getItem('low-score') > count){
+              gameContainer.innerHTML = `<span id="congrats" >GREAT JOB! </span> <span id="yourScore"> Congrats! You got the new best score of ${count} </span> <span onclick="window.location.reload()" id="reload"> New Game</span>`;
+              localStorage.setItem('low-score', count);
+            }
+
+        //if they did not get the new high score.
+            else{
+
+              if(localStorage.getItem('low-score') == null){
+                gameContainer.innerHTML = `<span id="congrats" >GOOD JOB! </span> <span id="yourScore"> Your score: ${count} </span> <span onclick="window.location.reload()" id="reload"> New Game</span> `;
+                localStorage.setItem('low-score', count);
+              }
+
+            else{
+            gameContainer.innerHTML = `<span id="congrats" >GOOD JOB! </span> <span id="yourScore"> Your score: ${count} <br> Best score: ${localStorage.getItem('low-score')} </span> <span onclick="window.location.reload()" id="reload"> New Game</span> `;}}
+
+            const congra = document.querySelector('#congrats');   
+            setInterval(function(){  
+              congra.style.color = randomRGB(); 
+            }, 1000);
+          }}         
+          }         
+        }
 
 //choosing random colors
 function randomRGB(){
@@ -142,3 +155,6 @@ const titled = document.querySelector('button');
 setInterval(function(){
   titled.style.color = randomRGB(); 
 }, 1000);
+
+
+
